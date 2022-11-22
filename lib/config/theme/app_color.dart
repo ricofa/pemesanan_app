97f94ae08582ddd240f3d660b81651f4f005704f
Rico Afandi <ricof.afandi@gmail.com>
2022-11-22 12:04:26 +0700

    #3 setup theme and font

---
+++ lib/config/theme/app_color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const Color inkBlack = Color(0xFF191D21);
+  static const Color inkGrey1 = Color(0xFF656F77);
+  static const Color inkGrey2 = Color(0xFFACB8C2);
+  static const Color inkGrey3 = Color(0xFFCDD9E3);
+  static const Color inkGrey4 = Color(0xFFE8EEF3);
+  static const Color inkWhite = Color(0xFFFFFFFF);
+
+  static const Color utlActive = Color(0xFF1814E4);
+  static const Color utlSuccess = Color(0xFF23E9B4);
+  static const Color utlInfo = Color(0xFF91D7E0);
+  static const Color utlWarning = Color(0xFFFFAC4B);
+  static const Color utlError = Color(0xFFFF5A5A);
+
+  static const Color acnLightBlue = Color(0xFFCBE3FF);
+  static const Color acnLightGreen = Color(0xFFD9FFF8);
+  static const Color acnLightPurple = Color(0xFFF2E8FF);
+  static const Color acnLightOrange = Color(0xFFFFD1B6);
+  static const Color acnLightPink = Color(0xFFFFDBF5);
+  static const Color acnLightYellow = Color(0xFFFFF3B6);
+}
