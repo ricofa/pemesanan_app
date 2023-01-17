57c60a687b2b2409f7dd632be1f97d2f78cbc698
Rico Afandi <ricof.afandi@gmail.com>
2023-01-17 15:34:45 +0700

    #7 Responsive view setup

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -1,2 +1,3 @@
 export 'theme_color.dart';
 export 'theme_font.dart';
+export 'theme_dimention.dart';
--- lib/utils/extension/sized_extension.dart
+++ lib/utils/extension/sized_extension.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 extension SizedExtension on double{
-  Widget get width => SizedBox(width: this);
-  Widget get height => SizedBox(height: this);
+  Widget get width => SizedBox(width: w);
+  Widget get height => SizedBox(height: h);
 }
